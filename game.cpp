b0fb711b38a8a5ad87a3d28c9bef5eb56d9baffe
Nura Programmer <53842620+Nura-Programmer@users.noreply.github.com>
2020-10-22 21:19:06 +0100

    Update game.cpp
    Comparing String with greater condition isnt the right decision, if i may propose.

--- game.cpp
+++ game.cpp
@@ -17,42 +17,38 @@ int main()
    long int s;
    time(&s);
   
+   string input;
    string time = ctime(&s);
    cout << "\t\t\t " << time << endl;
    
-   cout << (time <= "12")? "\t\t\t Good morining.." : "\t\t\t Good Day.." << endl; 
+   cout << (time <= stoi(12))? "\t\t\t Good morining.." : "\t\t\t Good Day.." << endl; 
    
    //object
    Game_Base_One game_base_one;
    Game_Base_Two game_base_two;
    Game_Base_Three game_base_three;
 
-
-   string input;
-
    cout << "Select Subject:" << endl;
    cout << "1 - Maths \n 2 - English \n 3 - coding \n >";
    cin >> input;
-
-
+   
    switch(input)
    {
-      case "1":
+      case "1": case 1:
          game_base_one.maths_board();
          game_base_one.maths_board_result();
       break;
-      case "2":
+      case "2": case 2:
          game_base_two.english_board();
          game_base_two.english_board_result();
       break;
-      case "3":
+      case "3": case 3:
          game_base_three.coding_board();
          game_base_three.coding_board_result();
       break;
       default:
          cout << "Wrong selection " << endl;
    }
-
 
   return 0;
 
